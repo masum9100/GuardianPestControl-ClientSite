839cd9f3de1979e5ce9257fd14a02fe04a8d679e
masum9100 <masum.seopro@gmail.com>
2023-11-05 19:12:16 +0600

    Nav and foorter added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { FaRegDotCircle } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            foot
+        <div className='bg-[#222222]'>
+            <footer className="footer p-10 text-white max-w-screen-xl mx-auto">
+                <aside>
+                    <img src="./logo.png" className='h-32' alt="" />
+                    <p><span className='font-bold'>Guardian Pest Control </span>in Miami. <br /> Safe for Kids & Pets, No Harmful, <br /> Smelly Chemicals! 100% Guaranteed.</p>
+                </aside>
+                <nav>
+                    <header className="footer-title text-[#55A839]">Services</header>
+                    <a className="link link-hover flex gap-1 items-center"> <FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle> ROACHES</a>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>RODENT</a>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>SPIDERS</a>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>FLYING INSECTS</a>
+                </nav>
+                <nav>
+                    <header className="footer-title text-[#55A839]">Company</header>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>About us</a>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>Contact</a>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>Jobs</a>
+                    <a className="link link-hover flex gap-1 items-center"><FaRegDotCircle className='text-[#55A839]'></FaRegDotCircle>Press kit</a>
+                </nav>
+                <nav>
+                    <header className="footer-title text-[#55A839]">Contact</header>
+                    <a className="link link-hover">14629 SW 104 Street #239 <br /> Miami Florida 33186</a>
+                    <a className="link link-hover">(712) 123-5375</a>
+                </nav>
+                
+
+            </footer>
+            <footer className="footer footer-center p-4 bg-[#222222] text-white">
+                <aside>
+                    <p>Copyright © 2023 - All right reserved by Guardian Pest Control</p>
+                </aside>
+            </footer>
         </div>
     );
 };
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,48 +1,74 @@
 import React from 'react';
+import { FaPhoneAlt } from "react-icons/fa";
 
 const NavBar = () => {
     return (
         <div className=''>
-            <div className="navbar bg-base-100">
+            <div className='bg-[#55AA39] text-white py-2'>
+                <div className='max-w-screen-xl mx-auto md:flex justify-between px-2 lg:px-0'>
+                    <div className='flex gap-2 items-center'>
+                        <p><FaPhoneAlt></FaPhoneAlt></p>
+                        <p>712-124-5375</p>
+                    </div>
+                    <div className='flex gap-2 items-center'>
+                        <p>EXTERIOR CHECKLIST</p>//
+                        <p>INTERIOR CHECKLIST</p>//
+                        <p>TIPS & TRICKS</p>
+                    </div>
+                </div>
+            </div>
+            <div className="navbar bg-base-100 max-w-screen-xl mx-auto text-center items-center">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+                            <li><a>Home</a></li>
+                            <li><a>All Service</a></li>
+                            <li><a>FAQS</a></li>
+                            <li><a>Contact Us</a></li>
                             <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                <a>Dashboard</a>
+                                <ul className="p-2 w-40">
+                                    <li><a>My-services</a></li>
+                                    <li><a>Add-services</a></li>
+                                    <li><a>My-schedules</a></li>
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <div >
+                        <img className='h-24' src="/logo.png" alt="" />
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
+                        <li className='hover:text-white'><a>Home</a></li>
+                        <li><a>All Service</a></li>
+                        <li><a>FAQS</a></li>
+                        <li><a>Contact Us</a></li>
                         <li tabIndex={0}>
                             <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                <summary>Dashboard</summary>
+                                <ul className="p-2 w-40">
+                                    <li><a>My-services</a></li>
+                                    <li><a>Add-services</a></li>
+                                    <li><a>My-schedules</a></li>
                                 </ul>
                             </details>
                         </li>
-                        <li><a>Item 3</a></li>
+
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="px-3 py-2 rounded-lg font-bold bg-[#55AA39] text-white hover:text-black">LogIn</a>
                 </div>
             </div>
+
         </div>
+
     );
 };
 
